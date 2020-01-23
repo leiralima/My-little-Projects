e134fa2280d1c7ad4b95f8400af3f5e3f9f7e88b
zelph <zelphgamer@gmail.com>
2020-01-23 13:21:21 -0300

    Added fucntion of numerical, clear, and exit buttons

--- Calculator1/Calculator1/MainWindow.xaml.cs
+++ Calculator1/Calculator1/MainWindow.xaml.cs
@@ -24,5 +24,70 @@ namespace Calculator1
         {
             InitializeComponent();
         }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text = "";
+        }
+
+        private void btnOne_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text = TextBox.Text + "1";
+        }
+
+        private void btnTwo_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text = TextBox.Text+"2";
+        }
+
+        private void btnThree_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text = TextBox.Text + "3";
+        }
+
+        private void btnFour_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text = TextBox.Text + "4";
+        }
+
+        private void btnFive_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text = TextBox.Text + "5";
+        }
+
+        private void btnSix_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text = TextBox.Text + "6";
+        }
+
+        private void btnSeven_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text = TextBox.Text + "7";
+        }
+
+        private void btnEight_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text = TextBox.Text + "8";
+        }
+
+        private void btnNine_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text = TextBox.Text + "9";
+        }
+
+        private void btnZero_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text = TextBox.Text + "0";
+        }
+
+        private void btnDot_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text = TextBox.Text + ".";
+        }
     }
 }
