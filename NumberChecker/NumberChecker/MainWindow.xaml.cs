ac402fce528b8ea7b58ffbcb506cb9d752c75636
zelph <zelphgamer@gmail.com>
2020-02-13 17:00:33 -0300

    Added check to see if a number is prime. Added to README the description of the new programs.

--- NumberChecker/NumberChecker/MainWindow.xaml.cs
+++ NumberChecker/NumberChecker/MainWindow.xaml.cs
@@ -26,34 +26,79 @@ namespace NumberChecker
             InitializeComponent();
         }
 
-        private void btnClose_Click(object sender, RoutedEventArgs e)
+        private void btnClose_Click(object sender, RoutedEventArgs e)//closes the program
         {
             this.Close();
         }
 
-        private void btnCheck_Click(object sender, RoutedEventArgs e)
+        private void writeLabelEven(string s)//writes on the label designated to the response of Even/Odd check
         {
-            if(float.TryParse(TextBox.Text, out num))
+            lbEven.Content = s;
+        }
+        private void writeLabelPrime(string s)//writes on the label designated to the response of Prime check
+        {
+            lbPrime.Content = s;
+        }
+        private void btnClear_Click(object sender, RoutedEventArgs e)//Clears all the boxes and labels
+        {
+            TextBox.Text = "";
+            writeLabelEven("");
+            writeLabelPrime("");
+        }
+        private void btnCheck_Click(object sender, RoutedEventArgs e)//Does all checks on the number typed
+        {
+            writeLabelEven("");
+            writeLabelPrime("");
+            if (float.TryParse(TextBox.Text, out num))//if the entry is valid, therefore no letters, special characters
             {
-                writeLabel("");
+                /*writeLabelEven("");
+                writeLabelPrime("");
                 num = num % 2;
                 if (num == 0)
                 {
-                    writeLabel("The number is even.");
+                    writeLabelEven("The number is even.");
                 }
                 else
                 {
-                    writeLabel("The number is odd.");
-                }
+                    writeLabelEven("The number is odd.");
+                }*/
+                checkEvenOdd(num);//operation to check if the number is even or odd
+                checkPrime(num);//operation to check if the number is prime
+            }
+            else//if the entry is not valid
+            {
+                writeLabelEven("Invalid entry. Please enter a valid number.");
+            }
+        }
+        private void checkEvenOdd(float n)
+        {
+            n = n % 2;
+            if (n == 0)
+            {
+                writeLabelEven("The number is even.");
             }
             else
             {
-                writeLabel("Invalid entry. Please enter a valid number.");
+                writeLabelEven("The number is odd.");
             }
         }
-        private void writeLabel(string s)
+        private void checkPrime(float n)
         {
-            lbCheck.Content = s;
+            bool prime = true;//boolean to show if the number is prime or not
+            float m = n / 2;//number that defines the maximum iterations of the for
+            for(int i = 2; i <= m; i++)
+            {
+                if (n % i == 0)
+                {
+                    writeLabelPrime("The number is not prime.");
+                    prime = false;
+                    break;
+                }
+            }
+            if (prime == true)
+            {
+                writeLabelPrime("The number is prime.");
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -3,3 +3,5 @@ This is my repository where I store the programs I create. Usually stuff I do to
 
 This is a summary of my programs so far:
 * Calcutalor1: a simple calculator that does the four basic mathematical operations.
+* ContactsBook: as of now, an idea for a contacts book.
+* NumberChecker: simple program that does various types of checks with a given number.
