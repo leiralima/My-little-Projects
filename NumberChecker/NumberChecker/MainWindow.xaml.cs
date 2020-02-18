cb8712a547ff95c828763ce25a2bbee7887ec585
zelph <zelphgamer@gmail.com>
2020-02-18 16:58:15 -0300

    Added check that shows dividends of a number. Numbers changed to int, there were no reason for then to be float. More comments added for better clarification of the functions.

--- NumberChecker/NumberChecker/MainWindow.xaml.cs
+++ NumberChecker/NumberChecker/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace NumberChecker
     /// </summary>
     public partial class MainWindow : Window
     {
-        private float num = 0;//number to be checked
+        private int num = 0;//number to be checked
         public MainWindow()
         {
             InitializeComponent();
@@ -43,30 +43,38 @@ namespace NumberChecker
         {
             lbPalind.Content = s;
         }
+        private void writeLabelFatorization(string s)//writes on the label designated to the response of Dividend check
+        {
+            lbFatorization.Content = s;
+            //lbFatorization.Content = lbFatorization.Content + s;
+        }
         private void btnClear_Click(object sender, RoutedEventArgs e)//Clears all the boxes and labels
         {
             TextBox.Text = "";
             writeLabelEven("");
             writeLabelPrime("");
             writeLabelPalind("");
+            writeLabelFatorization("");
         }
         private void btnCheck_Click(object sender, RoutedEventArgs e)//Does all checks on the number typed
         {
             writeLabelEven("");
             writeLabelPrime("");
             writeLabelPalind("");
-            if (float.TryParse(TextBox.Text, out num))//if the entry is valid, therefore no letters, special characters
+            writeLabelFatorization("");
+            if (int.TryParse(TextBox.Text, out num))//if the entry is valid, therefore no letters, special characters
             {
                 checkEvenOdd(num);//operation to check if the number is even or odd
                 checkPrime(num);//operation to check if the number is prime
                 checkPalindrome(num);//operation to check if the number is palindrome
+                checkFatorization(num);//operation to show the dividends of a number
             }
             else//if the entry is not valid
             {
                 writeLabelEven("Invalid entry. Please enter a valid number.");
             }
         }
-        private void checkEvenOdd(float n)
+        private void checkEvenOdd(int n)//Check that verifies if given number is odd or even
         {
             n = n % 2;
             if (n == 0)
@@ -78,10 +86,10 @@ namespace NumberChecker
                 writeLabelEven("The number is odd.");
             }
         }
-        private void checkPrime(float n)
+        private void checkPrime(int n)//Check that verifies if given number is prime
         {
             bool prime = true;//boolean to show if the number is prime or not
-            float m = n / 2;//number that defines the maximum iterations of the for
+            int m = n / 2;//number that defines the maximum iterations of the for
             for(int i = 2; i <= m; i++)
             {
                 if (n % i == 0)
@@ -96,11 +104,10 @@ namespace NumberChecker
                 writeLabelPrime("The number is prime.");
             }
         }
-        private void checkPalindrome(float n)
+        private void checkPalindrome(int n)//Check that verifies if given number is a palindrome
         {
             int r, sum = 0, aux;
-            aux = (int)n;
-            //int.TryParse(n, out aux);
+            aux = n;
             while (aux > 0)
             {
                 r = aux % 10;
@@ -115,6 +122,22 @@ namespace NumberChecker
             {
                 writeLabelPalind("The number is not a palindrome.");
             }
+        }
+        private void checkFatorization(int n)//Check gives the prime factors of a given number
+        {
+            int aux1, aux2 = n;
+            String st="";
+            for (aux1 = 2; aux2 > 1; aux1++)
+            {
+                int x = 0;
+                if (aux2 % aux1 == 0)
+                {
+                    aux2 /= aux1;
+                    x++;
+                    st = st + aux1.ToString() + " is a prime factor " + x.ToString() + " times!\n";
+                }
+            }
+            writeLabelFatorization(st);
         }
     }
 }
