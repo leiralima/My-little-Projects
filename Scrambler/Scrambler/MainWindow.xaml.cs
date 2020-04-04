c7fd24c7bde02ec5e3dfbeabd2f4f1589d9e37a8
zelph <zelphgamer@gmail.com>
2020-04-03 21:13:01 -0300

    Fuction to scramble and unscramble made, layout changed and added a clear button

--- Scrambler/Scrambler/MainWindow.xaml.cs
+++ Scrambler/Scrambler/MainWindow.xaml.cs
@@ -29,5 +29,70 @@ namespace Scrambler
         {
             this.Close();
         }
+
+        private void btnScr_Click(object sender, RoutedEventArgs e)
+        {
+            var builder = new StringBuilder();//variable for building a string
+            string s1, s2;//string to input
+            //This sets the string with the given text and cleans the third text box
+            s1 = TxtBox1.Text;
+            s2 = TxtBox2.Text;
+            TxtBox3.Text = "";
+            //Checks which string is bigger to be used as limit for the FOR operation
+            int index = 0;
+            if (s1.Length > s2.Length)
+            {
+                index = s1.Length;
+            }
+            else
+            {
+                index = s2.Length;
+            }
+            for (int i = 0; i < index; i++)//FOR operation that builds the new string
+            {
+                if (i < s1.Length)
+                {
+                    builder.Append(s1[i]);
+                }
+                if (i < s2.Length)
+                {
+                    builder.Append(s2[i]);
+                }//Both IF check if the given string still has characters left. If they do it is integrated into the final string
+            }
+            TxtBox3.Text = builder.ToString();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            TxtBox1.Text = "";
+            TxtBox2.Text = "";
+            TxtBox3.Text = "";
+        }
+
+        private void btnUnscr_Click(object sender, RoutedEventArgs e)
+        {
+            var builder1 = new StringBuilder();
+            var builder2 = new StringBuilder();//variables for building a string
+            string str;//string to input
+            //This sets the strings with the given texts and cleans the first and second text boxes
+            str = TxtBox3.Text;
+            TxtBox1.Text = "";
+            TxtBox2.Text = "";
+            //Sets the limit to the FOR operation
+            int index = str.Length;
+            for (int i = 0; i < index; i++)//FOR operation that builds the new string
+            {
+                if (i % 2 == 0)
+                {
+                    builder1.Append(str[i]);
+                }
+                else
+                {
+                    builder2.Append(str[i]);
+                }
+            }
+            TxtBox1.Text = builder1.ToString();
+            TxtBox2.Text = builder2.ToString();
+        }
     }
 }
